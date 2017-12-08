676f63214fff0c7876f026a32e9e3b5d94cc5c0c
Miithos <lionloki10@gmail.com>
2017-12-07 23:07:49 -0700

    catalogo

--- public/js/productosCat.js
+++ public/js/productosCat.js
@@ -20,9 +20,9 @@ function consultarProductos(){
         $.each(data, function (i, item)
         {   
             trHTML += 
-            '<div class="col-sm-6 col-md-4">  <div class="thumbnail" >  <div class="row"> <div class="col-md-6 col-xs-6">'  + item.producto + 
-            '</div> <div class="col-md-6 col-xs-6 "> <h3> <label> $'  + item.precio + 
-            '</label></h3> </div> </div> <p>' + item.descripcion +
+            '<div class="col-sm-6 col-md-4">  <div class="thumbnail" >  <img src="data:image/jpg;base64,'+item.imagen+'" alt="" style="height:300px;" /> <div class="row"> <div class="col-md-6 col-xs-6"><h3>'  + item.producto + 
+            '</h3></div> <div class="col-md-6 col-xs-6 "> <h3> <label> $'  + item.precio + 
+            '/'+item.cantidad+'Kg</label></h3> </div> </div> <p>' + item.descripcion +
             '</p> </div> </div> ';   
         });
 
@@ -31,4 +31,5 @@ function consultarProductos(){
     });
 
 }
+
 
--- resources/views/catalogo.blade.php
+++ resources/views/catalogo.blade.php
@@ -1,4 +1,4 @@
-
+<link href="{{ asset('css/catalogo.css') }}" rel="stylesheet">
 @extends('layouts.app')
 
 @section('content') 
@@ -13,10 +13,17 @@
         </div>
 
         <div class="panel-body" id="tbProductos">
+        	<div align="center">
+        		<h3><Strong>Tomate.com ofrece una gran variedad de productos de alta calidad. Utilizamos las mejores tecnicas de cuidado natural para dar a nuestros clientes tomates ricos y llenos de nutrientes.</Strong></h3>
+        	</div>
+        <br>
+        <br>
+
                   
-      
                    
         </div>
+
+
     </div>
 </div>
 
